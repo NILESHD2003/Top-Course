8f5976ca8e5b64de888593152ca09e716846ed54
Nilesh Deshpande <nileshdeshpandework@gmail.com>
2024-01-19 00:03:34 +0530

    fixed deployment issue

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="bg-bgDark py-4 text-white text-center text-normal font-bold"><div>Developed by Nilesh Deshpande</div><div className="text-blue-500"><a href="https://nileshdeshpande.netlify.app/" target="_blank">nileshdeshpande.netlify.app</a></div></div>
+    <div className="bg-bgDark py-4 text-white text-center text-normal font-bold"><div>Developed by Nilesh Deshpande</div><div className="text-blue-500"><a href="https://nileshdeshpande.netlify.app/" target="_blank" rel="noopener noreferrer">nileshdeshpande.netlify.app</a></div></div>
   )
 }
 
